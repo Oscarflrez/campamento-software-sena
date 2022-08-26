4b6df3be6752b246fd23507f26b3b2d36a4dd36b
oaflorez <oaflorez78@misena.edu.co>
2022-08-26 09:43:08 -0500

    migraciones

--- backend/database/migrations/2022_08_26_134755_create_bootcamps_table.php
+++ backend/database/migrations/2022_08_26_134755_create_bootcamps_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('bootcamps', function (Blueprint $table) {
             $table->id();
+            $table->string('name',100);
+            $table->string('website',70);
+            $table->string('phone',30);
+            $table->text('description');
+            $table->float('average_rating',2, 1);
+            $table->float('average_cost',8, 2);
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
